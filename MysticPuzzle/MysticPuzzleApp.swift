da07982e947a23c034e14e8b2fc2e4c18fa30351
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-23 17:32:33 +0300

    Initial Commit

---
+++ MysticPuzzle/MysticPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysticPuzzleApp.swift
+//  MysticPuzzle
+//
+//  Created by mehmet Çelik on 23.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MysticPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
