fb2b39a61e093da429a9695e98e32e8486236c5f
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-24 18:59:33 +0300

    3

--- MysticPuzzle/MysticPuzzleView.swift
+++ MysticPuzzle/MysticPuzzleView.swift
@@ -10,126 +10,79 @@ import SwiftUI
 
 
 
-
 struct MysticPuzzleView: View {
+    @StateObject var mysticPuzzleViewModel: MysticPuzzleViewModel = MysticPuzzleViewModel()
     
-    @StateObject var mysticPuzzleViewModel : MysticPuzzleViewModel = MysticPuzzleViewModel()
-    let tileDimensions:CGFloat = 70.0
     let title = "Mystic Puzzle"
     let newGamePrompt = "New Game"
     let shufflePrompt = "Shuffle"
-    let easyGamePromt = "Easy Game"
+    let goalPrompt = "Easy Game"
+    let tileDimensions: CGFloat = 70
+    let speakerDimensions: CGFloat = 40
+    
+    @State var volume: Float = 1.0
+    
+    // Remove
+    @State private var moveCount = 0
+    // Remove
+    
+    
+    
     var body: some View {
-     
-        ZStack {
-            Image("WaterBackground")
-                .opacity(0.9).ignoresSafeArea()
-            
-            VStack {
-               
+        VStack {
                 
                 ChalkboardTextView(text: title, size: 42, color: .white)
                 
-                ZStack {
-                  
-                    let last = mysticPuzzleViewModel.mysticPuzzleModel.tiles.count-1
-                    ForEach(0..<last,id: \.self) { index in
-                        let tile = mysticPuzzleViewModel.mysticPuzzleModel.tiles[index]
-                        TileView(tileNumber: tile.value, tileDimensions: tileDimensions, offset: tile.currentPoint)
-                            .onTapGesture {
-                                withAnimation(.easeOut(duration: 0.2)) {
-                                    if mysticPuzzleViewModel.move(index: index) {
-                                        print("move tile!")
-                                    }
-                                }
-                            }
-                           
-                        
-                    }
-                  
-                }
-                .frame(maxWidth: tileDimensions * 4, maxHeight: tileDimensions * 4)
-                .padding()
-                .background(
-                    Image("BlueMarbleBackground")
+                puzzleBoard
+                
+                buttons
+                
+            HStack(spacing: 20) {
+                
+                Slider(value: $volume, in: 0...1)
+                
+                Button(action: {
+                    volume = volume > 0 ? 0 : 1
+                }, label: {
+                    Image(systemName: volume > 0 ? "speaker" : "speaker.slash")
                         .resizable()
-                        .opacity(0.9)
-                        
-                )
-                .cornerRadius(20)
-                .padding()
-                .padding(.vertical)
-                .shadow(color: .black, radius: 10,x: 3, y: 3)
-                
-                HStack {
-                    Button(
-                        action: {
-                            mysticPuzzleViewModel.createInitialItems()
-                            mysticPuzzleViewModel.shuffle()
-                        },
-                        label: {
-                            ChalkboardTextView(
-                                text:mysticPuzzleViewModel.done() ? newGamePrompt : shufflePrompt,
-                                size: 24,
-                                color: .yellow
-                            )
-                            .padding()
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(.yellow,lineWidth: 3)
-                            )
-                            .padding(.vertical)
-                        })
-                    Button(
-                        action: {
-                            mysticPuzzleViewModel.createInitialItems()
-                            mysticPuzzleViewModel.shuffle()
-                        },
-                        label: {
-                            ChalkboardTextView(
-                                text: easyGamePromt,
-                                size: 24,
-                                color: .green
-                            )
-                            .padding()
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(.yellow,lineWidth: 3)
-                            )
-                            .padding(.vertical)
-                        })
-                    
-                    
-                }
-               
-                
-            }
-            
-        }
-    }
-}
+                        .foregroundColor(.white)
+                        .scaledToFit()
+                    .frame(width: speakerDimensions, height: speakerDimensions)
+                })
 
+            }.padding()
+                .padding(.vertical)
+            
+            }
+        .background(Image("WaterBackground"))
+        }
+}
 
 struct ChalkboardTextView: View {
     let text: String
     let size: CGFloat
     let color: Color
     var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .font(Font.custom("ChalkboardSE-Bold", size: size))
+        Text( text)
+            .font(Font.custom( "ChalkboardSE-Bold", size: size))
             .fontWeight(.bold)
-            .foregroundColor(color)
+            .foregroundColor( color)
             .shadow(color: color, radius: 1)
     }
 }
 
 struct TileView: View {
     let tileNumber: Int
-    let tileDimensions:CGFloat
-    let offset:CGPoint
-    let somePadding : CGFloat = 3
-    let digitResizeFactor: CGFloat = 0.8
+    let tileDimensions: CGFloat
+    let offset: CGPoint
+    
+    let digitResizeFactor: CGFloat = 2/5
+    
+    
+    // NEW DRAG
+    @State private var dragOffset = CGSize.zero
+    // NEW DRAG
     
     var body: some View {
         let direction = offset * tileDimensions
@@ -137,28 +90,119 @@ struct TileView: View {
         let deltaX = direction.x + tileDimensions / 2
         let deltaY = direction.y - 3 * tileDimensions / 2
         
+        let somePadding: CGFloat = 3
         
-        
-       
-            Image( "digit\(tileNumber)")
+        Image( "digit\(tileNumber)")
+            .resizable()
+            .renderingMode( .template)
+            .foregroundColor( .red)
+            .aspectRatio(contentMode: .fit)
+            .frame(
+                width: tileDimensions*digitResizeFactor,
+                height: tileDimensions*digitResizeFactor)
+            .scaleEffect( tileNumber < 10 ? 1 : 1.4)
+            .background(
+            Image("MarbleTileBackground")
                 .resizable()
-                .renderingMode(.template)
-                .foregroundColor(.red)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: tileDimensions * 0.8 , height: tileDimensions * 0.8 )
-                .scaleEffect(tileNumber < 10  || tileNumber == 11 ? 0.8 : 1)
-                .padding()
-                .background(
-                  Image("MarbleTileBackground")
-                        .resizable()
-                        .frame(width: tileDimensions - somePadding,height: tileDimensions - somePadding)
-                        .cornerRadius(10)
-                )
-                .offset(x: deltaX, y: deltaY)
-                .shadow(color: .black, radius: 1 , x: 1, y :1 )
+                .opacity( 0.9)
+                .frame(width: tileDimensions - somePadding, height: tileDimensions - somePadding)
+                .cornerRadius( 10)
+            ).padding()
         
+//    https://www.youtube.com/watch?v=BKxbHnka4-Q
+        
+//            .offset(x: mysticPuzzleViewModel.movable( index: i) ? deltaX+dragOffset.width : deltaX,
+//                    y: deltaY+dragOffset.height)
+            .offset(x: deltaX+dragOffset.width,
+                    y: deltaY+dragOffset.height)
+
+            .shadow(color: .black, radius: 1, x: 1, y: 1)
+        // New gesture
+            .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    print("Dragging")
+                    dragOffset = gesture.translation
+                }
+                .onEnded { _ in
+                    withAnimation {
+                        dragOffset = .zero
+                    }
+                }
+            )
+            
+        // New gesture
     }
 }
+
+
+extension MysticPuzzleView {
+    private var buttons: some View {
+        HStack {
+            Button(action: {
+                mysticPuzzleViewModel.createInitialItems()
+                mysticPuzzleViewModel.shuffle()
+            }) {
+                ChalkboardTextView(text: mysticPuzzleViewModel.done() ? newGamePrompt : shufflePrompt, size: 24, color: .yellow)
+                    .padding()
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke( .yellow, lineWidth: 3)
+                    )
+                    .padding(.vertical)
+                    .animation( .easeInOut, value: mysticPuzzleViewModel.done())
+        }
+            
+            Button(action: {
+                mysticPuzzleViewModel.createInitialItems()
+            }) {
+                ChalkboardTextView(text: goalPrompt, size: 24, color: .green)
+                    .padding()
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke( .green, lineWidth: 3)
+                    )
+                    .padding(.vertical)
+            }
+
+        }
+    }
+}
+
+extension MysticPuzzleView {
+    private var puzzleBoard: some View {
+        ZStack {
+            
+            
+            ForEach( 0..<mysticPuzzleViewModel.mysticPuzzleModel.tiles.count-1, id: \.self) { i in
+                TileView(tileNumber: mysticPuzzleViewModel.mysticPuzzleModel.tiles[i].value,
+                         tileDimensions: tileDimensions,
+                         offset: mysticPuzzleViewModel.mysticPuzzleModel.tiles[i].currentPoint)
+                    .onTapGesture {
+                        withAnimation(.easeOut(duration: 0.2)){
+                            if mysticPuzzleViewModel.move( index: i) {
+                                moveCount += 1
+                                print("\(moveCount) moves")
+                                
+                                playSound(sound: "mixkit-glass-plate-slide-1527", type: "wav", volume: volume)
+                            }
+                        }
+                    }
+            }
+        }
+        .frame(maxWidth: tileDimensions * 4, maxHeight: tileDimensions * 4)
+        .padding()
+        .background(
+            Image("BlueMarbleBackground")
+                .resizable().opacity( 0.9)
+        )
+        .shadow(color: .black, radius: 10, x: 3, y: 3)
+        .padding()
+
+    }
+}
+
+
 
 
 #Preview {
--- MysticPuzzle/MysticPuzzleViewModel.swift
+++ MysticPuzzle/MysticPuzzleViewModel.swift
@@ -10,219 +10,233 @@ import UIKit
 
 
 
+import AVFoundation
+var audioPlayer: AVAudioPlayer? = AVAudioPlayer()
 
 
-
+// ViewModel
 class MysticPuzzleViewModel: ObservableObject {
-    @Published var mysticPuzzleModel: MysticPuzzleModel = MysticPuzzleModel(tiles: [], n: 0)
+    @Published var mysticPuzzleModel: MysticPuzzleModel = MysticPuzzleModel(tiles: [], n: 0) {
+        didSet {
+            saveItems()
+        }
+    }
     
+    
+    let itemsKey = "Tile list"
     init() {
-//        self.findFonts()
-        self.createInitialItems()
-        self.shuffle()
+        guard
+            let data = UserDefaults.standard.data(forKey: itemsKey),
+            let savedItems =
+                try? JSONDecoder().decode(MysticPuzzleModel.self, from: data)
+        else
+        {
+            self.createInitialItems()
+            self.shuffle()
+            return
+        }
+        self.mysticPuzzleModel = savedItems
     }
     
-/// font değiştirmeyi kullanmadık ama değiştirmek istersek kullanabiliriz diye bunu buraya bıraktım
-   
-//    func findFonts() {
-//        for family in UIFont.familyNames.sorted(){
-//            let names = UIFont.fontNames(forFamilyName: family)
-//            print("Family: \(family) Font names: \(names)")
-//        }
-//    }
-//  
-    
-    
+    func saveItems() {
+        if let encodeData = try? JSONEncoder().encode( mysticPuzzleModel) {
+            UserDefaults.standard.set( encodeData, forKey: itemsKey)
+        }
+    }
     func createInitialItems() {
+
         var tiles: [TileModel] = []
-       
-        tiles
-            .append(
-                TileModel(
-                    value: 1,
-                    currentPoint: LEFT * 2,
-                    winningPoint: LEFT * 2
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 2,
-                    currentPoint: LEFT * 1,
-                    winningPoint: LEFT * 1
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 3,
-                    currentPoint: .zero,
-                    winningPoint: .zero
-                )
-            )
-        
-        tiles
-            .append(
-                TileModel(
-                    value: 4,
-                    currentPoint: RIGHT * 1,
-                    winningPoint: RIGHT * 1
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 5,
-                    currentPoint: LEFT * 2 + DOWN,
-                    winningPoint: LEFT * 2 + DOWN
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 6,
-                    currentPoint: LEFT * 1 + DOWN,
-                    winningPoint: LEFT * 1 + DOWN
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 7,
-                    currentPoint: DOWN,
-                    winningPoint: DOWN
-                )
-            )
-        
-        tiles
-            .append(
-                TileModel(
-                    value: 8,
-                    currentPoint: RIGHT * 1 + DOWN,
-                    winningPoint: RIGHT * 1 + DOWN
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 9,
-                    currentPoint: LEFT * 2 + DOWN * 2,
-                    winningPoint: LEFT * 2 + DOWN * 2
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 10,
-                    currentPoint: LEFT * 1 + DOWN * 2,
-                    winningPoint: LEFT * 1 + DOWN * 2
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 11,
-                    currentPoint: DOWN * 2,
-                    winningPoint: DOWN * 2
-                )
-            )
-        
-        tiles
-            .append(
-                TileModel(
-                    value: 12,
-                    currentPoint: RIGHT * 1 + DOWN * 2,
-                    winningPoint: RIGHT * 1 + DOWN * 2
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 13,
-                    currentPoint: LEFT * 2 + DOWN * 3,
-                    winningPoint: LEFT * 2 + DOWN * 3
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 14,
-                    currentPoint: LEFT * 1 + DOWN * 3,
-                    winningPoint: LEFT * 1 + DOWN * 3
-                )
-            )
-        tiles
-            .append(
-                TileModel(
-                    value: 15,
-                    currentPoint: DOWN * 3,
-                    winningPoint: DOWN * 3
-                )
-            )
         tiles.append(
-            TileModel(
-                value: 16,
-                currentPoint: DOWN * 3 + RIGHT,
-                winningPoint: DOWN * 3 + RIGHT
-            )
+        TileModel(value: 1,
+                  currentPoint: LEFT * 2,
+                  winningPoint: LEFT * 2)
         )
- 
+        tiles.append(
+        TileModel(value: 2,
+                  currentPoint: LEFT * 1,
+                  winningPoint: LEFT * 1)
+        )
+        tiles.append(
+        TileModel(value: 3,
+                  currentPoint: .zero,
+                  winningPoint: .zero)
+        )
         
+        tiles.append(
+        TileModel(value: 4,
+                  currentPoint: RIGHT * 1,
+                  winningPoint: RIGHT * 1)
+        )
+        tiles.append(
+        TileModel(value: 5,
+                  currentPoint: LEFT * 2 + DOWN,
+                  winningPoint: LEFT * 2 + DOWN)
+        )
+        tiles.append(
+        TileModel(value: 6,
+                  currentPoint: LEFT * 1 + DOWN,
+                  winningPoint: LEFT * 1 + DOWN)
+        )
+        tiles.append(
+        TileModel(value: 7,
+                  currentPoint: DOWN,
+                  winningPoint: DOWN)
+        )
+        
+        tiles.append(
+        TileModel(value: 8,
+                  currentPoint: RIGHT * 1 + DOWN,
+                  winningPoint: RIGHT * 1 + DOWN)
+        )
+        tiles.append(
+        TileModel(value: 9,
+                  currentPoint: LEFT * 2 + DOWN * 2,
+                  winningPoint: LEFT * 2 + DOWN * 2)
+        )
+        tiles.append(
+        TileModel(value: 10,
+                  currentPoint: LEFT * 1 + DOWN * 2,
+                  winningPoint: LEFT * 1 + DOWN * 2)
+        )
+        tiles.append(
+        TileModel(value: 11,
+                  currentPoint: DOWN * 2,
+                  winningPoint: DOWN * 2)
+        )
+        
+        tiles.append(
+        TileModel(value: 12,
+                  currentPoint: RIGHT * 1 + DOWN * 2,
+                  winningPoint: RIGHT * 1 + DOWN * 2)
+        )
+        tiles.append(
+        TileModel(value: 13,
+                  currentPoint: LEFT * 2 + DOWN * 3,
+                  winningPoint: LEFT * 2 + DOWN * 3)
+        )
+        tiles.append(
+        TileModel(value: 14,
+                  currentPoint: LEFT * 1 + DOWN * 3,
+                  winningPoint: LEFT * 1 + DOWN * 3)
+        )
+        tiles.append(
+        TileModel(value: 15,
+                  currentPoint: DOWN * 3,
+                  winningPoint: DOWN * 3)
+        )
+        tiles.append(
+        TileModel(value: 16,
+                  currentPoint: RIGHT * 1 + DOWN * 3,
+                  winningPoint: RIGHT * 1 + DOWN * 3)
+        )
+
         let n = 4
-        self.mysticPuzzleModel = MysticPuzzleModel(tiles: tiles, n: n)
+        
+        
+        self.mysticPuzzleModel = MysticPuzzleModel(
+            tiles: tiles,
+            n: n)
     }
-    
     func done() -> Bool {
+        
+        for tile in mysticPuzzleModel.tiles {
+            if tile.currentPoint != tile.winningPoint {
+                return false
+            }
+        }
+        
         return true
     }
     
-    func shuffle() {
+    // NEW DRAG
+    func movable( index: Int) -> Bool {
         
         let n = mysticPuzzleModel.n
+        let emptyPosition =
+        mysticPuzzleModel.tiles[n*n-1].currentPoint
         
+        let tilePosition = mysticPuzzleModel.tiles[index].currentPoint
+        let distance = tilePosition.distanceTo( point: emptyPosition)
+        
+        return distance == 1
+    }
+    // NEW DRAG
+    
+    
+    func move( index: Int) -> Bool {
+        var result = false
+        let n = mysticPuzzleModel.n
+        let emptyPosition =
+        mysticPuzzleModel.tiles[n*n-1].currentPoint
+        
+        let tilePosition = mysticPuzzleModel.tiles[index].currentPoint
+        let distance = tilePosition.distanceTo( point: emptyPosition)
+
+        if distance == 1 {
+            let saveTilePoint = mysticPuzzleModel.tiles[index].currentPoint
+            
+            mysticPuzzleModel.tiles[index] = TileModel(
+                value: mysticPuzzleModel.tiles[index].value,
+                currentPoint: emptyPosition,
+                winningPoint: mysticPuzzleModel.tiles[index].winningPoint)
+            
+            // Update empty tile position
+            mysticPuzzleModel.tiles[n*n-1].currentPoint = saveTilePoint
+            
+            result = true
+        }
+        
+        return result
+    }
+    
+    // 1 2 3 4 5 6 8 7 9 10 11 12 13 14 15
+    func shuffle() {
+        // If n is even do even number of inversions
+        // If n is odd do an odd number of inversions
+        let n = mysticPuzzleModel.n
         for _ in 0..<n * n {
-            let (i,j) = getRandomIJ(max: n*n - 1)
+            let (i,j) = getRandomIJ(max: n * n - 1)
             let lastPosition = mysticPuzzleModel.tiles[i].currentPoint
-            
             mysticPuzzleModel.tiles[i].currentPoint = mysticPuzzleModel.tiles[j].currentPoint
-            
             mysticPuzzleModel.tiles[j].currentPoint = lastPosition
         }
         
     }
     
-    func getRandomIJ(max: Int) -> (Int,Int) {
+    /// Returns a pair of two different integers i and j
+    /// between 0 and max (exclusive)
+    /// - Parameter max: max is the maximal possible integer
+    /// to be returned
+    /// - Returns: Returns (i,j) where i is not equal to j
+    /// and 0 <= i,j < max. If max = 1 then
+    /// (1,1) is returned.
+    func getRandomIJ( max: Int) -> (Int,Int) {
         if max == 1 {
             return (1,1)
         }
-        let i = Int.random(in: 0...max)
+        let i = Int.random(in: 0..<max)
         var j: Int
         repeat {
-            j = Int.random(in: 0...max)
-        }while (i==j )
+            j = Int.random(in: 0..<max)
+        } while ( i == j)
         return (i,j)
     }
-    
-    func move(index: Int) -> Bool {
-        var result = false
-        
-        let n = mysticPuzzleModel.n
-        let emptyPosition = mysticPuzzleModel.tiles[n * n - 1].currentPoint
-        let tilePosition = mysticPuzzleModel.tiles[index].currentPoint
-        let distance = tilePosition.distanceTo(point: emptyPosition)
-        
-        if distance == 1 {
-            mysticPuzzleModel.tiles[index] = TileModel(
-                value: mysticPuzzleModel.tiles[index].value,
-                currentPoint: emptyPosition,
-                winningPoint: mysticPuzzleModel.tiles[index].winningPoint
-            )
-            mysticPuzzleModel.tiles[n * n - 1].currentPoint = tilePosition
-            
-            result = true
-            
-        }
-        
-        return result
-    }
 }
+
+// Play sound with default volume value
+// set to 1.0
+func playSound(sound: String, type: String, volume: Float = 1.0) {
+            if let path = Bundle.main.path(forResource: sound, ofType: type) {
+                do {
+                    audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+                    
+                    // Set volume
+                    audioPlayer?.setVolume(volume, fadeDuration: 0.1)
+                    
+                    audioPlayer?.play()
+                } catch {
+                    print("ERROR")
+                }
+            }
+        }
+
